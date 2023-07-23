b7d1e3e3698bf8374790696cee79f990ea842168
It-Shu <it.shustov@gmail.com>
2023-07-23 18:21:08 +0300

    map added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.scss';
-import {Map} from './components/Map';
-
+import {Map} from './components/Map'
 function App() {
     return (
         <div className={'app-container'}>
-            
+
             <div className={'routes-container'}>
                 <div>Маршрут №1</div>
                 <div>Маршрут №2</div>
                 <div>Маршрут №3</div>
-
             </div>
-        <Map/>
-    </div>);
+
+            <Map/>
+
+        </div>);
 }
 
 export default App;
